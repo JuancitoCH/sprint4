ead57708b5b0b3462187c64061cee9585c8ff61f
Juancito <cjuan.chona@hotmail.com>
2022-07-01 15:40:31 -0300

    Creacion de archivo vacio

---
+++ creation.py
@@ -0,0 +1,31 @@
+import csv
+
+def creation(name):
+    try:
+        with open(f'{name}.csv') as archivo:
+            print('File Already Exist...')
+            formatFile = csv.reader(archivo)
+            for lines in formatFile:
+                print(lines)
+            chose = input('ingrese y para sobrescribir, de lo contrario n :')
+            if chose=='y': raise Exception
+            archivo.close()
+            return {'exist':True,'stop':True}
+    except:
+        print('creating...')
+        with open(f'{name}.csv','w',newline='') as archivo:
+            csv.writer(archivo).writerow([
+                '',
+                'NroCheque',
+                'CodigoBanco',
+                'CodigoScurusal',
+                'NumeroCuentaOrigen',
+                'Valor',
+                'FechaOrigen',
+                'FechaPago',
+                'DNI',
+                'Estado',
+                'Tipo'])
+            archivo.close()
+            print('created successfully')
+            return {'created':True,'stop':False}
---
+++ listado_chesques.py
@@ -0,0 +1,15 @@
+import csv
+from creation import creation
+
+creation('cheques')
+
+# NroCheque
+# CodigoBanco
+# CodigoScurusal
+# NumeroCuentaOrigen
+# NumeroCuentaDestino
+# Valor
+# FechaOrigen
+# FechaPago
+# DNI
+# Estado
